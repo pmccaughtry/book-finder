0414db47940425a2e273673ceebd675ac19d8edc
Paul McCaughtry <paul.mccaughtry@gmail.com>
2024-12-11 14:52:04 -0700

    Task: Added configuation for static site output

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
-    reactStrictMode: true,
-    rewrites: async () => {
-        return [
-            {
-                source: "/api/:path*",
-                destination:
-                    process.env.NODE_ENV === "development"
-                        ? "http://127.0.0.1:8000/api/:path*"
-                        : "/api/",
-            },
-        ];
+    images: {
+        unoptimized: true,
     },
+    output: 'export',
+    reactStrictMode: true,
+    // rewrites: async () => {
+    //     return [
+    //         {
+    //             source: "/api/:path*",
+    //             destination:
+    //                 process.env.NODE_ENV === "development"
+    //                     ? "http://127.0.0.1:8000/api/:path*"
+    //                     : "/api/",
+    //         },
+    //     ];
+    // },
 };
 
 export default nextConfig;
