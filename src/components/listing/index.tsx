2ce07538abc330cbdf277996a714b2c34fed6eda
Paul McCaughtry <paul.mccaughtry@gmail.com>
2024-12-10 22:29:17 -0700

    Task: Gracefully handle missing data
    - Added ternary checks to user a value if returned in the results, otherwise use fallback values to denote data not available

--- src/components/listing/index.tsx
+++ src/components/listing/index.tsx
@@ -1,5 +1,4 @@
 import type { ReactNode } from 'react';
-import Image from "next/image";
 
 export interface Result {
     id: string;
@@ -18,13 +17,14 @@ export interface Result {
 
 export default function Listing({ result }: { result: Result }): ReactNode {
     const { authors, description, imageLinks, title } = result.volumeInfo;
+    const thumbnail = imageLinks?.thumbnail ? imageLinks.thumbnail : 'https://placehold.co/128x161?text=Image+Not+Found';
 
     return (
         <div className="bg-white rounded-lg shadow p-4 hover:shadow-md transition-shadow">
             <h3 className="text-lg font-semibold text-gray-800 pb-4">The Pragmatic Programmer</h3>
-            <img src={imageLinks.thumbnail} alt={`Book Cover - ${title}`} />
-            <p className="text-gray-600 mt-2"><strong>Author(s):</strong> {authors.join(', ')}</p>
-            <p className="text-gray-600 mt-2"><strong>Description:</strong> {description}</p>
+            <img src={thumbnail} alt={`Book Cover - ${title}`} />
+            <p className="text-gray-600 mt-2"><strong>Author(s):</strong> {authors ? authors.join(', ') : 'Author(s) not available.'}</p>
+            <p className="text-gray-600 mt-2"><strong>Description:</strong> {description ? description : 'Description not available.'}</p>
         </div>
     );
 }
