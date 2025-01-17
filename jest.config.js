eb1941de0107f0cbd94b852094fa0d2a1f5c58fe
Paul McCaughtry <paul.mccaughtry@gmail.com>
2025-01-17 11:33:38 -0700

    Task: updated configuration files due to failing tests after moving test and setupTests.ts to <rootdir>/__tests__

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,16 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    transform: {
-        '^.+\\.(ts|tsx)$': 'ts-jest',
-    },
-    moduleNameMapper: {
-        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-        '^@/(.*)$': '<rootDir>/src/$1',
-    },
-    setupFilesAfterEnv: ['@testing-library/jest-dom']
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
+
+module.exports = createJestConfig(customJestConfig);
